54fa8541f7922827af455e01d0683f4b0c5bba80
Isaias Pomales <impomales@gmail.com>
2018-07-23 17:06:13 -0400

    Html layout (#1)
    * install express
    
    * set up basic server
    
    * set up static html file
    
    * add img logos for footer links
    
    * define header and footer
    
    * add class names and ids to header and footer
    
    * change size of logos

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
