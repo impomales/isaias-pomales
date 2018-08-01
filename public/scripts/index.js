4bd74165bbb06900d64f14b24ecc7812db789653
Isaias Pomales <impomales@gmail.com>
2018-08-01 19:07:22 -0400

    Javascript (#8)
    * can click header buttons to jump to sections
    
    * can send an email using contact form
    
    * refactor code for changing views

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,14 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
     contact: document.getElementById('contact')
   };
 
-  for (let i = 0; i < headerBtns.length; i++) {
-    headerBtns[i].addEventListener('click', () => {
-      const id = headerBtns[i].id.split('-')[0];
+  let curr = 'home';
 
-      for (let section in sections) {
-        if (section === id) sections[section].classList.remove('hidden');
-        else sections[section].classList.add('hidden');
-      }
-    });
+  const changeViews = evt => {
+    const id = evt.target.id.split('-')[0];
+
+    sections[curr].classList.add('hidden');
+    sections[id].classList.remove('hidden');
+
+    curr = id;
+  };
+
+  for (let i = 0; i < headerBtns.length; i++) {
+    headerBtns[i].addEventListener('click', changeViews);
   }
 });
